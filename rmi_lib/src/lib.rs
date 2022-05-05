62508cee0ecc5a31e4e4dba2c51777f3e96a6d74
Edgar Geier <egeier@rhrk.uni-kl.de>
2022-05-05 23:48:21 +0200

    Export TrainedRMII struct

--- rmi_lib/src/lib.rs
+++ rmi_lib/src/lib.rs
@@ -1,12 +1,12 @@
+mod cache_fix;
 mod codegen;
 mod models;
 mod train;
-mod cache_fix;
 
 pub mod optimizer;
-pub use models::{RMITrainingData, RMITrainingDataIteratorProvider, ModelInput};
-pub use models::KeyType;
-pub use optimizer::find_pareto_efficient_configs;
-pub use train::{train, train_for_size, train_bounded};
-pub use codegen::rmi_size;
 pub use codegen::output_rmi;
+pub use codegen::rmi_size;
+pub use models::KeyType;
+pub use models::{ModelInput, RMITrainingData, RMITrainingDataIteratorProvider};
+pub use optimizer::find_pareto_efficient_configs;
+pub use train::{train, train_bounded, train_for_size, TrainedRMI};

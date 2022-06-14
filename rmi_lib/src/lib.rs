8d6ee2fa195eab2729af016f0cac97cc1124351e
Edgar Geier <egeier@rhrk.uni-kl.de>
2022-06-14 14:38:37 +0200

    Export TrainingKey trait

--- rmi_lib/src/lib.rs
+++ rmi_lib/src/lib.rs
@@ -7,6 +7,6 @@ pub mod optimizer;
 pub use codegen::rmi_size;
 pub use codegen::{output_rmi, output_rmis};
 pub use models::KeyType;
-pub use models::{ModelInput, RMITrainingData, RMITrainingDataIteratorProvider};
+pub use models::{ModelInput, RMITrainingData, RMITrainingDataIteratorProvider, TrainingKey};
 pub use optimizer::find_pareto_efficient_configs;
 pub use train::{train, train_bounded, train_for_size, TrainedRMI};
